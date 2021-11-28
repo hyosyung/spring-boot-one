fbeb4f8cfa667b84a41c85416ffbc8e8954753ea
hyosung <melon940925@naver.com>
2021-11-28 19:16:11 +0900

    @Transactional

--- src/main/java/com/coupang/jayden/service/DatabaseService.java
+++ src/main/java/com/coupang/jayden/service/DatabaseService.java
@@ -5,6 +5,7 @@ import com.coupang.jayden.model.CatImageVO;
 import com.coupang.jayden.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -15,10 +16,12 @@ import java.util.stream.Collectors;
 public class DatabaseService {
     private final ImageRepository imageRepository;
 
+    @Transactional
     public void deleteByIds(List<Long> idList) {
         imageRepository.deleteAllById(idList);
     }
 
+    @Transactional
     public void save(List<String> urls) {
         List<CatImage> images = urls.stream()
                 .filter(Objects::nonNull)
@@ -27,6 +30,7 @@ public class DatabaseService {
         imageRepository.saveAll(images);
     }
 
+    @Transactional(readOnly = true)
     public List<CatImageVO> getAllImages() {
         return imageRepository.findAll()
                 .stream()
